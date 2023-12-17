66e3a2808db68b08aa2916f6c2721314593ee777
tulugkucukogut <tulugkucukogut@gmail.com>
2023-12-17 17:27:32 +0300

    adding form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
 from flask import Flask
 from flask import Flask, render_template
 
@@ -11,13 +11,23 @@ class HomePage(MethodView):
 
 class BillFormPage(MethodView):
     def get(self):
-        return render_template('bill_form_page.html')
+        bill_form = BillForm()
+        return render_template('bill_form_page.html',billform=bill_form)
 
 class ResultsPage(MethodView):
     pass
+ 
+class BillForm(Form):
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
 
-class BillFprm(Form):
-    pass
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house:")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house:")
+
+    submit = SubmitField("Calculate")
 
 app.add_url_rule("/", view_func= HomePage.as_view('home_page'))
 app.add_url_rule("/bill", view_func= BillFormPage.as_view('bill_form_page'))
