e82ca12ed5efc300aad6a4261860e7a2ca97e333
tulugkucukogut <tulugkucukogut@gmail.com>
2023-12-18 17:42:36 +0300

    Adding result page and css

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flat_mate import flat
 
 app = Flask(__name__)
 
@@ -12,24 +13,33 @@ class HomePage(MethodView):
 class BillFormPage(MethodView):
     def get(self):
         bill_form = BillForm()
+        
         return render_template('bill_form_page.html',billform=bill_form)
 
+
 class ResultsPage(MethodView):
-    pass
+    def post(self):
+        billform = BillForm(request.form)
+        the_bill = flat.Bill(float(billform.amount.data),billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data,float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data,float(billform.days_in_house2.data))
+        return render_template("results.html", name1 = flatmate1.name, amount1 = flatmate1.pays(the_bill,flatmate2),
+                                            name2 = flatmate2.name, amount2 = flatmate2.pays(the_bill,flatmate1))
  
 class BillForm(Form):
-    amount = StringField("Bill Amount: ")
-    period = StringField("Bill Period: ")
+    amount = StringField("Bill Amount: ", default="100")
+    period = StringField("Bill Period: ", default="December 2020")
 
-    name1 = StringField("Name: ")
-    days_in_house1 = StringField("Days in the house:")
+    name1 = StringField("Name: ", default="Andrea")
+    days_in_house1 = StringField("Days in the house:", default="10")
 
-    name2 = StringField("Name: ")
-    days_in_house2 = StringField("Days in the house:")
+    name2 = StringField("Name: ", default="Reea")
+    days_in_house2 = StringField("Days in the house:", default="20")
 
     submit = SubmitField("Calculate")
 
 app.add_url_rule("/", view_func= HomePage.as_view('home_page'))
 app.add_url_rule("/bill", view_func= BillFormPage.as_view('bill_form_page'))
+app.add_url_rule("/results", view_func= ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
