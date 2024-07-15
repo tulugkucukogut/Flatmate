fc88ac6ab4ec0bcecc7b95423908b7c52ccbe163
tulugkucukogut <cumhuriye.kucukogut@vestel.com.tr>
2024-07-15 15:22:10 +0300

    Js file is imported

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    alert("XXX");
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        // toggle the icon
+        this.classList.toggle('bx-show');
+        this.classList.toggle('bx-hide');
+    });
+});
